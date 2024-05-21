b1eea3de95082988b4475744b5cfca9db1ed69aa
willianxzy <willianlittig14@gmail.com>
2024-05-21 11:56:09 -0300

    AllArgsConstructor, ToString

--- aula1005/src/main/java/com/itilh/aula/aula1005/model/ModelTeste.java
+++ aula1005/src/main/java/com/itilh/aula/aula1005/model/ModelTeste.java
@@ -1,13 +1,18 @@
 package com.itilh.aula.aula1005.model;
 
 import java.math.BigInteger;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ModelTeste {
     private BigInteger id;
     private String nome;
